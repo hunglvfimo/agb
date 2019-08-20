690cc0db6a14e1d0a626a8b12f9211b2a7fee5a8
hunglv <hunglv@fimo.edu.vn>
2019-08-20 23:44:37 +0900

    add file

---
+++ generate.bat
@@ -0,0 +1,3 @@
+rem python generate_dataset.py --ground_truth D:\Workspace\data\raw\NAMDINH\ground_truth_agb\AGB_ground_truth.shp --image_dir D:\Workspace\data\raw\NAMDINH\images
+python generate_dataset.py --ground_truth D:\Workspace\data\raw\NAMDINH\ground_truth_lc\gt_10.tif --image_path D:\Workspace\data\raw\NAMDINH\images\all.tif --save_dir D:\Workspace\data\raw\NAMDINH\lc_patches
+\NAMDINH\lc
